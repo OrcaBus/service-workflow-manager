d18c1375974d9a80bec2ad063f505e73697ec487
Ray Liu <ray.liu@umccr.org>
2025-04-29 12:47:01 +1000

    test fixed for app

--- app/workflow_manager_proc/tests/case.py
+++ app/workflow_manager_proc/tests/case.py
@@ -12,17 +12,17 @@ class WorkflowManagerProcUnitTestCase(TestCase):
 
     def setUp(self) -> None:
         # Mock boto3 client
-        self.events = botocore.session.get_session().create_client('events', region_name='ap-southeast-2')
-        self.boto3_patcher = patch('emit_workflow_run_state_change.client', return_value=self.events)
+        self.events_client = botocore.session.get_session().create_client('events', region_name='ap-southeast-2')
+        self.boto3_patcher = patch('workflow_manager_proc.services.emit_workflow_run_state_change.client', return_value=self.events_client)
         self.mock_boto3 = self.boto3_patcher.start()
         self.mock_events = self.mock_boto3.return_value
 
-        self.events_stubber = Stubber(self.events)
-        self.events_stubber.activate()
+        self.events_client_stubber = Stubber(self.events_client)
+        self.events_client_stubber.activate()
         super().setUp()
 
     def tearDown(self) -> None:
-        self.events_stubber.deactivate()
+        self.events_client_stubber.deactivate()
         self.boto3_patcher.stop()
         super().tearDown()
 
