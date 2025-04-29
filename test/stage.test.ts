3839e9db218331b977382059b0a8cf8d91f9ca45
Ray Liu <ray.liu@umccr.org>
2025-04-29 11:35:10 +1000

    fix iac test issue

--- test/stage.test.ts
+++ test/stage.test.ts
@@ -17,6 +17,10 @@ describe('cdk-nag-stateless-toolchain-stack', () => {
     // Pick the prod environment to test as it is the most strict
     // ...getStackProps('PROD'),
     ...getWorkflowManagerStackProps('PROD'),
+    env: {
+      account: '123456789',
+      region: 'ap-southeast-2',
+    },
   });
 
   Aspects.of(deployStack).add(new AwsSolutionsChecks());
