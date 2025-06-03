6f323db7dfd5f981f02c9c7b7f86c6fd1cd4cb1e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-06-03 16:25:10 +1000

    Add migration, fix imports

--- app/workflow_manager/models/__init__.py
+++ app/workflow_manager/models/__init__.py
@@ -1,12 +1,15 @@
 # https://docs.djangoproject.com/en/4.1/topics/db/models/#organizing-models-in-a-package
 
-from .workflow import Workflow
-from .payload import Payload
-from .workflow_run import WorkflowRun, LibraryAssociation
-from .library import Library
 from .analysis import Analysis
-from .analysis_run import AnalysisRun
 from .analysis_context import AnalysisContext
-from .workflow_run_comment import WorkflowRunComment
+from .analysis_run import AnalysisRun
 from .analysis_run_readset import AnalysisRunReadset
 from .analysis_run_state import AnalysisRunState
+from .common import Status
+from .library import Library
+from .payload import Payload
+from .state import State
+from .utils import WorkflowRunUtil
+from .workflow import Workflow
+from .workflow_run import WorkflowRun, LibraryAssociation
+from .workflow_run_comment import WorkflowRunComment
