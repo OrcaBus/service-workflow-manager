a9b2218ee88604b4065ca22b6d51cb5471c2a7b0
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-08-31 13:49:26 +1000

    Add support for hashing payload data

--- app/workflow_manager_proc/services/event_utils.py
+++ app/workflow_manager_proc/services/event_utils.py
@@ -1,5 +1,7 @@
 import boto3
 import logging
+import rfc8785
+import hashlib
 from enum import Enum
 from workflow_manager_proc.domain.event import arsc, wrsc
 
@@ -47,3 +49,18 @@ def emit_event(event_type: EventType, event_bus: str, event_json):
     logger.info(event_json)
     logger.info(f"{__name__} done.")
     return response
+
+
+def hash_payload_data(data: dict) -> str:
+    """
+    Generates a unique hash for the JSON represented as dict.
+    First generates the canonical JSON following RFC8785. Then calculates the SHA256 (hex) digest.
+    Args:
+        data: the dict representing the JSON payload. Has to be JSON serializable.
+
+    Returns: a hash of the input.
+
+    """
+    data_canonical = rfc8785.dumps(data)
+    data_hash = hashlib.sha256(data_canonical).hexdigest()
+    return data_hash
