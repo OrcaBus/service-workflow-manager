fed484c65e1bce995073d2bb00dbed2b09e78634
Ray Liu <ray.liu@umccr.org>
2026-02-18 14:47:56 +1100

    fix the histiry type

--- app/workflow_manager/serializers/workflow.py
+++ app/workflow_manager/serializers/workflow.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from workflow_manager.models import Workflow
@@ -29,6 +30,7 @@ class WorkflowListSerializer(WorkflowBaseSerializer):
 
     history = serializers.SerializerMethodField()
 
+    @extend_schema_field(WorkflowHistorySerializer(many=True))
     def get_history(self, obj):
         history_records = self.context.get("history_map", {}).get(obj.orcabus_id, [])
         return WorkflowHistorySerializer(history_records, many=True).data
